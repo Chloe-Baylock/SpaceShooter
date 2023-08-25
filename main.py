62e3b5d06b42798b56cb9f032245d1bb4521f327
unknown <chloebox523@gmail.com>
2023-08-24 21:28:25 -0400

    click to start a swing

--- main.py
+++ main.py
@@ -15,35 +15,35 @@ class Player():
   def __init__(self):
     self.x = 0
     self.y = 0
-    self.velX = 0
-    self.velY = 0
-    self.accelX = 0
-    self.accelY = 0
+    self.weapon = "greatSword"
+    self.isSwinging = False
 
   def move(self, mousePos):
     dirX = mousePos[0] - self.getX()
     dirY = mousePos[1] - self.getY()
 
     if abs(dirX) <= pixelSize and abs(dirY) <= pixelSize:
-      print('yes')
       return
 
-    unitMove = self.unitVector((dirX,dirY))
+    unitMove = self.unitVector(mousePos)
 
     if (mousePos[0] and mousePos[1]):
       #check that it is not empty
-      newX = unitMove[0] + self.getX()
-      newY = unitMove[1] + self.getY()
+      newX = unitMove[0] * pixelSize + self.getX()
+      newY = unitMove[1] * pixelSize + self.getY()
 
       self.setX(newX * 1)
       self.setY(newY * 1)
       
-  def unitVector(self, direction):
-    x = direction[0]
-    y = direction[1]
+  def unitVector(self, mousePos):
+    x = mousePos[0] - self.getX()
+    y = mousePos[1] - self.getY()
 
-    if (y == 0):
-      return
+    # x = direction[0]
+    # y = direction[1]
+
+    if (x == 0):
+      return ((0,0))
     
     alpha = math.atan(y/x)
     if (x < 0 and y < 0):
@@ -53,7 +53,7 @@ class Player():
     unitX = math.cos(alpha)
     unitY = math.sin(alpha)
 
-    return ((unitX * pixelSize, unitY * pixelSize))
+    return ((unitX, unitY))
 
   def setX(self, val):
     self.x = val
@@ -66,7 +66,15 @@ class Player():
 
   def getY(self):
     return self.y
-    
+
+  def swing(self, weapon):
+    self.isSwinging = True
+  
+  def getIsSwinging(self):
+    return p.isSwinging
+
+  def setIsSwinging(self, val):
+    p.isSwinging = val
 
 
     
@@ -78,14 +86,21 @@ while running:
   # poll for events
   # pygame.QUIT event means the user clicked X to close your window
   for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-          running = False
-
-  mousePosition = pygame.mouse.get_pos()
-  p.move(mousePosition)
+    if event.type == pygame.QUIT:
+      running = False
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+      p.setIsSwinging(True)
 
   # fill the screen with a color to wipe away anything from last frame
   screen.fill("black")
+
+  mousePosition = pygame.mouse.get_pos()
+  if p.getIsSwinging():
+    x = p.getX() + p.unitVector(mousePosition)[0] * pixelSize
+    y = p.getY() + p.unitVector(mousePosition)[1] * pixelSize
+    pygame.draw.rect(screen, "orange", (x,y, 64, 64))
+  else:
+    p.move(mousePosition)
 
   # RENDER YOUR GAME HERE
   pygame.draw.rect(screen, "white", (p.getX(), p.getY(), 64, 64))
