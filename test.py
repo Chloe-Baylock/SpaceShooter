49c86fa24593299ac5057b5fbf2ee22afd603682
unknown <chloebox523@gmail.com>
2023-09-14 12:21:02 -0400

    will try to blit in main code now

--- test.py
+++ test.py
@@ -8,6 +8,12 @@ clock = pygame.time.Clock()
 player_surf = pygame.Surface((50,50))
 player_surf.fill("red")
 player_rect = player_surf.get_rect(center = (300,300))
+player_mask = pygame.mask.from_surface(player_surf)
+
+obstacle_surf = pygame.Surface((400,400))
+obstacle_surf.fill("cyan")
+obstacle_rect = obstacle_surf.get_rect(center = (300,300))
+obstacle_mask = pygame.mask.from_surface(obstacle_surf)
 
 while True:
   for event in pygame.event.get():
@@ -15,16 +21,21 @@ while True:
       pygame.quit()
       sys.exit()
     
-  screen.fill('white')
+  screen.fill('gray')
 
   if pygame.mouse.get_pos():
     player_rect.center = pygame.mouse.get_pos()
 
+  screen.blit(obstacle_surf,obstacle_rect)
   screen.blit(player_surf, player_rect)
+  
+  offset_x = obstacle_rect.left - player_rect.left
+  offset_y = obstacle_rect.top - player_rect.top
+
+  if player_mask.overlap(obstacle_mask,(offset_x,offset_y)):
+    player_surf.fill('purple')
+  else:
+    player_surf.fill('red')
+
   pygame.display.update()
   clock.tick(60)
-
-
-
-# player_mask = pygame.mask.from_surface(player_surf)
-# obstacle_mask = pygame.mask.from_surface(obstacle_surf)
